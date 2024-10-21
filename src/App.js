707d840cf040f8dcfb5ff8d4107e9fea84efc636
Rajesh K <rajeshkrk1996@gmail.com>
2024-10-21 10:53:18 +0530

    Add App files
    Add main App files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './TicTacToe';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>TicTacToe Game</h1>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
