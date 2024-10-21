041d8ce1a21f4f20ef0042addf94838d6a38d672
Rajesh K <rajeshkrk1996@gmail.com>
2024-10-21 09:25:55 +0530

    Implement Player Turns
    Implement Player Turns in Tic Tac Toe board

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TicTacToe.css';
 
 const TicTacToe = () => {
-  return (
-    <div className='game'>
-        <div className='board'>
-            {Array(9).fill(null).map((_, index) => (
-                <button key={index} className='square'></button>
-            )) }
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(true);
+
+    const handleClick = (index) => {
+        if (board[index]) return;
+        const newBoard = board.slice();
+        newBoard[index] = isXNext ? 'X' : 'O';
+        setBoard(newBoard);
+        setIsXNext(!isXNext);
+    }
+  
+    return (
+        <div className='game'>
+            <div className='board'>
+                {board.map((value, index) => (
+                    <button key={index} className='square' onClick={() => handleClick(index)}>{value}</button>
+                )) }
+            </div>
         </div>
-    </div>
   );
 };
 
